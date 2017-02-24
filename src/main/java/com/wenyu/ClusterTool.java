c2869c38fc0b32ab4880c570f85616eba723a295
Wenyu <changwy_1987@hotmail.com>
2017-02-24 16:42:19 +0800

    Initial version of cassandra cluster tools

---
+++ src/main/java/com/wenyu/ClusterTool.java
@@ -0,0 +1,17 @@
+package com.wenyu;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+/**
+ * Created by wenyu on 2/24/17.
+ */
+public class ClusterTool {
+
+    public static void main(String[] args) {
+        List<Class<? extends Runnable>> commands = newArrayList(
+
+        );
+    }
+}
