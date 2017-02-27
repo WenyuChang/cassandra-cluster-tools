3b26eb8589f3416b5ef9a7e4c90ecb737bf90040
Wenyu <changwy_1987@hotmail.com>
2017-02-27 17:10:19 +0800

    Skeleton of cassandra cluster tools

--- src/main/java/com/wenyu/ClusterTool.java
+++ src/main/java/com/wenyu/ClusterTool.java
@@ -1,17 +1,59 @@
 package com.wenyu;
 
+import com.google.common.base.Throwables;
+import com.wenyu.clustertools.ClusterToolCmd;
+import com.wenyu.clustertools.Status;
+import io.airlift.command.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
+
 
 /**
  * Created by wenyu on 2/24/17.
  */
 public class ClusterTool {
 
-    public static void main(String[] args) {
-        List<Class<? extends Runnable>> commands = newArrayList(
+    private static List<Class<? extends Runnable>> commands() {
+        List<Class<? extends Runnable>> commands = new ArrayList<>();
 
-        );
+        commands.add(Help.class);
+        commands.add(Status.class);
+
+        return commands;
+    }
+
+    public static void main(String[] args) {
+        Cli.CliBuilder<Runnable> builder = Cli.builder("clustertool");
+
+        builder.withDescription("Manage your Cassandra cluster")
+                .withDefaultCommand(Help.class)
+                .withCommands(commands());
+        Cli<Runnable> parser = builder.build();
+
+        int status = 0;
+        try
+        {
+            Runnable parse = parser.parse(args);
+            parse.run();
+        } catch (IllegalArgumentException |
+                IllegalStateException |
+                ParseArgumentsMissingException |
+                ParseArgumentsUnexpectedException |
+                ParseOptionConversionException |
+                ParseOptionMissingException |
+                ParseOptionMissingValueException |
+                ParseCommandMissingException |
+                ParseCommandUnrecognizedException e)
+        {
+            status = 1;
+        } catch (Throwable throwable)
+        {
+            status = 2;
+        }
+
+        System.exit(status);
     }
 }
---
+++ src/main/java/com/wenyu/clustertools/ClusterToolCmd.java
@@ -0,0 +1,108 @@
+package com.wenyu.clustertools;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+import com.google.common.base.Throwables;
+import io.airlift.command.*;
+import org.apache.cassandra.tools.NodeProbe;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by wenyu on 2/24/17.
+ */
+public abstract class ClusterToolCmd implements Runnable {
+    @Option(type = OptionType.GLOBAL, name = {"-nf", "--nodes-file"},
+            description = "Path to the nodes connection information file")
+    private String nodesConnectionFile = EMPTY;
+
+    protected List<Node> nodes;
+
+    private void parseNodesConnectionFile() {
+        nodes = new ArrayList<Node>();
+        if (StringUtils.isEmpty(nodesConnectionFile.trim())) {
+            // Add local
+            nodes.add(new Node());
+            return;
+        }
+
+        try {
+            File nodesFile = new File(nodesConnectionFile);
+            Scanner scanner = new Scanner(nodesFile).useDelimiter(System.lineSeparator());
+            while (scanner.hasNext())
+            {
+                String jmxRole = scanner.next();
+                String serverRole = jmxRole.split("/")[0];
+                String authRole = EMPTY;
+                if (jmxRole.split("/").length > 1) {
+                    authRole = jmxRole.split("/")[1];
+                }
+
+                // Make sure server information is correctly formatted
+                assert serverRole.length() > 0;
+
+                Node node = new Node();
+                node.server = serverRole.split(":")[0];
+                if (serverRole.split(":").length > 1) {
+                    node.port = serverRole.split(":")[1];
+                }
+
+                if (!EMPTY.equals(authRole)) {
+                    node.username = authRole.split(":")[0];
+                    node.password = authRole.split(":")[1];
+                }
+
+                nodes.add(node);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (AssertionError e) {
+            throw e;
+        }
+    }
+
+    public void run() {
+        parseNodesConnectionFile();
+        if (nodes == null || nodes.size() <= 0) {
+            return;
+        }
+
+        execute();
+    }
+
+    public abstract void execute();
+
+    protected NodeProbe connect(Node node)
+    {
+        NodeProbe nodeClient = null;
+
+        try {
+            if (node.username.isEmpty())
+                nodeClient = new NodeProbe(node.server, parseInt(node.port));
+            else
+                nodeClient = new NodeProbe(node.server, parseInt(node.port), node.username, node.password);
+
+        } catch (IOException | SecurityException e) {
+            Throwable rootCause = Throwables.getRootCause(e);
+            System.err.println(format("nodetool: Failed to connect to '%s:%s' - %s: '%s'.", node.server, node.port, rootCause.getClass().getSimpleName(), rootCause.getMessage()));
+            System.exit(1);
+        }
+
+        return nodeClient;
+    }
+
+    public static class Node {
+        private String server = "127.0.0.1";
+        private String port = "7199";
+        private String username = EMPTY;
+        private String password = EMPTY;
+    }
+}
